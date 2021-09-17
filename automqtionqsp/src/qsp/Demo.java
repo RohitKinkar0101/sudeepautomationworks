0bcd39d4b451674e9b15a61beb5bd1c7ae6bc8b7
RohitKinkar0101 <90815518+RohitKinkar0101@users.noreply.github.com>
2021-09-17 10:13:36 +0530

    Update Demo.java
    update1

--- automqtionqsp/src/qsp/Demo.java
+++ automqtionqsp/src/qsp/Demo.java
@@ -3,7 +3,7 @@ package qsp;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println(" happy HAPPY GANESH CHATURTHI");
+		System.out.println(" happy HAPPY GANESH CHATURTHI Sudeep");
 
 	}
 
